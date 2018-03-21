f4141476192891af2035e961776e5cf420cb3257
Yoel García Carrera <Darzakeshdaw@gmail.com>
2018-03-21 09:17:33 +0100

    Clase Terminada

--- Juegolandia/src/conecta4/Tablero.java
+++ Juegolandia/src/conecta4/Tablero.java
@@ -11,131 +11,121 @@ package conecta4;
  */
 public class Tablero {
 
-    //Tamaño del tablero
-    private int m_numColumnas;
-    private int m_numFilas;
+	// Tamaño del tablero
+	private int m_numColumnas;
+	private int m_numFilas;
 
-    //Datos del tablero
-    private int m_tablero[][];
+	// Datos del tablero
+	private int m_tablero[][];
 
+	/**
+	 * Constructor del tablero
+	 * 
+	 * @param anchura
+	 *            Anchura del tablero
+	 * @param altura
+	 *            Altura del tablero
+	 */
+	public Tablero(int anchura, int altura) {
+		// Tamaño del tablero
+		m_numColumnas = anchura;
+		m_numFilas = altura;
 
-    /**
-     * Constructor del tablero
-     * @param anchura Anchura del tablero
-     * @param altura Altura del tablero
-     */
-    public Tablero(int anchura, int altura)
-    {
-        //Tamaño del tablero
-        m_numColumnas = anchura;
-        m_numFilas = altura;
+		// Crea el tablero
+		m_tablero = new int[m_numFilas][m_numColumnas];
 
-        //Crea el tablero
-        m_tablero = new int[m_numFilas][m_numColumnas];
+		// Inicializa el tablero
+		for (int i = 0; i < m_numFilas; i++) {
+			for (int j = 0; j < m_numColumnas; j++)
+				m_tablero[i][j] = 0;
+		}
 
-        //Inicializa el tablero
-        for (int i=0; i<m_numFilas; i++)
-        {
-            for(int j=0; j<m_numColumnas; j++)
-                m_tablero[i][j] = 0;
-        }
+	}
 
-    }
+	/**
+	 * Constsructor de Copia del tablero
+	 * 
+	 * @param original
+	 *            Tablero del cual realizar la copia
+	 */
+	public Tablero(Tablero original) {
+		// Tamaño del tablero
+		m_numColumnas = original.m_numColumnas;
+		m_numFilas = original.m_numFilas;
 
-    /**
-     * Constsructor de Copia del tablero
-     * @param original Tablero del cual realizar la copia
-     */
-    public Tablero (Tablero original)
-    {
-        //Tamaño del tablero
-        m_numColumnas = original.m_numColumnas;
-        m_numFilas = original.m_numFilas;
+		// Crea el tablero
+		m_tablero = new int[m_numFilas][m_numColumnas];
 
-        //Crea el tablero
-        m_tablero = new int[m_numFilas][m_numColumnas];
+		// Inicializa el tablero
+		for (int i = 0; i < m_numFilas; i++) {
+			for (int j = 0; j < m_numColumnas; j++)
+				m_tablero[i][j] = original.m_tablero[i][j];
+		}
+	}
 
-        //Inicializa el tablero
-        for (int i=0; i<m_numFilas; i++)
-        {
-            for(int j=0; j<m_numColumnas; j++)
-                m_tablero[i][j] = original.m_tablero[i][j];
-        }
-    }
+	/**
+	 * Devuelve el número de columnas
+	 */
+	public int numColumnas() {
+		return m_numColumnas;
 
-    /**
-     * Devuelve el número de columnas
-     */
-    public int numColumnas()
-    {
-        return m_numColumnas;
+	}
 
-    }
+	/**
+	 * Devuelve el número de filas
+	 */
+	public int numFilas() {
+		return m_numFilas;
 
-    /**
-     * Devuelve el número de filas
-     */
-    public int numFilas()
-    {
-        return m_numFilas;
+	}
 
-    }
+	/**
+	 * Indica si existe una ficha en la posición indicada.
+	 */
+	public boolean existeFicha(int i, int j) {
+		if (m_tablero[i][j] != 0)
+			return true;
+		else
+			return false;
+	}
 
-    /**
-     * Indica si existe una ficha en la posición indicada.
-     */
-    public boolean existeFicha(int i, int j)
-    {
-        if(m_tablero[i][j] != 0)
-            return true;
-        else
-            return false;
-    }
+	/**
+	 * Devuelve a quién pertenece la casilla especificada. 0 está vacía 1
+	 * pertenece al jugador 1 2 pertenece al jugador 2
+	 */
+	public int obtenerCasilla(int i, int j) {
+		return m_tablero[i][j];
+	}
 
-    /**
-     * Devuelve a quién pertenece la casilla especificada.
-     * 0 está vacía
-     * 1 pertenece al jugador 1 
-     * 2 pertenece al jugador 2 
-     */
-    public int obtenerCasilla(int i, int j)
-    {
-        return m_tablero[i][j];
-    }
+	/**
+	 * Cambia el valor de una casilla del tablero
+	 */
+	public void cambiarCasilla(int i, int j, int valor) {
+		if (i >= 0 && i < m_numFilas && j >= 0 && j < m_numColumnas)
+			m_tablero[i][j] = valor;
+	}
 
-    /**
-     * Cambia el valor de una casilla del tablero
-     */
-    public void cambiarCasilla(int i, int j, int valor)
-    {
-        if(i>=0 && i<m_numFilas && j>=0 && j<m_numColumnas)
-            m_tablero[i][j] = valor;
-    }
+	// Limpia el tablero. Deja todas las casillas vacías.
+	public void limpiarTablero() {
+		for (int i = 0; i < m_numFilas; i++)
+			for (int j = 0; j < m_numColumnas; j++)
+				m_tablero[i][j] = 0;
+	}
 
-    //Limpia el tablero. Deja todas las casillas vacías.
-    public void limpiarTablero()
-    {
-        for(int i=0; i<m_numFilas; i++)
-            for(int j=0; j<m_numColumnas;j++)
-                m_tablero[i][j] = 0;
-    }
+	/**
+	 * Indica si el tablero está lleno.
+	 */
+	public boolean tableroLleno() {
+		boolean lleno = true;
 
+		for (int i = 0; ((i < m_numFilas) && lleno); i++)
+			for (int j = 0; ((j < m_numColumnas) && lleno); j++)
+				if (m_tablero[i][j] == 0)
+					lleno = false;
+		return lleno;
+	}
 
-    /**
-     * Indica si el tablero está lleno.
-     */
-    public boolean tableroLleno()
-    {
-        boolean lleno = true;
-
-        for(int i=0;((i<m_numFilas) && lleno);i++)
-            for(int j=0;((j<m_numColumnas) && lleno);j++)
-                if(m_tablero[i][j] == 0)
-                    lleno = false;
-        return lleno;
-    }
-
-    /**
+	/**
      * Comprueba si se ha hecho 4 en raya.
      * Realiza la búsqueda en horizontal, en vertical y en diagonal.
      * 0 no hay 4 en raya.
@@ -185,4 +175,81 @@ public class Tablero {
                     if(i+3 < m_numFilas)
                     {
                         //Realiza una búsqueda en la primera diagonal
-                       
+                    	if(j-3>=0)
+                    	{
+                    		if((m:tablero[i+1][j-1]==casilla)&&(m_tablero[i+2][j-2]==casilla&&(m_tablero[i+3][j+3]==casilla)))
+                    	}
+                    	ganador=casilla;
+                    	fin = true;
+                    }
+                }
+                //Realizar una b�squeda en la segunda diagonal
+                if(j+3<m_numColumnas)
+                {
+                	if((m_tablero[i+1][j+1]==casilla)&&(m_tablero[i+2][j+2]==casilla)&&(m_tablero[i+3][j+3]==casilla))
+                }
+                ganador = casilla;
+                fin=true;
+            }
+        }
+    }
+}j++;}i++;}return ganador;}
+/**
+ * Inserta una ficha en el tablero, en la columna especificada.Devuelve:
+ * 0 si la ficha se inserta correctamente.
+ * 1 si la columna en la que se intenta colocar est� llena.
+ * -1 en caso de error
+ * @param columna Indica la columna en la que se coloca la ficha
+ * @param jugador Indica el jugador que est� jugando, 1 para jugador 1, 2 para jugador 2
+ */
+	
+public int ponerficha(int columna, int jugador)
+{
+	boolean encontrado=false;
+	//Comprueba que la columna pasada como par�metro sea correcta
+	if((columna<0)||(columna>=m_numColumnas))
+		return -1;
+	//Comprueba la primera casila libre de esa columna(si tiene)
+	for(int i=0;(i<m_numfilas && !encontrado);i++)
+	{
+		if(m_tablero[i][columna]==0)
+		{encontrado = true;
+		//Si encuentra una casilla libre, pone la ficha
+		m_tablero[i][columna]=jugado;
+		}
+	}
+	//Si cuando sale del for no ha encontrado ninguna casilla libre es que la columna est� llena
+	if(!encontrado)
+		return 1;
+	else
+		return 0;
+}
+/**
+ * Comprueba si la columna que se le pasa como par�metro es correcta para una jugada.
+ * Devuelve la fila en la que se le colocar�a la ficha y -1 en caso de error
+ * @param columna
+ * @return
+ */
+public int comprobarColumna(int columna)
+{
+	int fila = -1;
+	boolean encontrado = false;
+	//Comprueba que la columna pasada como par�metro sea correcta
+	if((columna<0)||(columna>=m_numColumnas))
+	{
+		encontrado=true;
+	}
+	//Comprueba la primera caslla libre de esa columna(si tiene)
+	for(int i =0;(i<m_numFilas && !encontrado);i++)
+	{
+		if(m_tablero[i][columna]==0)
+		{fila = i;
+		encontrado = true;
+		}
+	}
+	return fila;
+}
+}
+		}
+	}
+}
