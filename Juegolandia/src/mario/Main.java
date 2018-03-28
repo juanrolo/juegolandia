54cb5ea16ff687fcc59bcd1a633f55a04dedc5aa
joserodalorenzo <jroda1997@gmmail.com>
2018-03-28 18:47:44 +0200

    Clase main modificada

--- Juegolandia/src/mario/Main.java
+++ Juegolandia/src/mario/Main.java
@@ -66,4 +66,89 @@ public class Main extends Stage {
 		sounds.startLoader();
 		
 		// Iniciamos el mapa.
-		
+		map.initMap();
+		// creamos un jugador
+		Mario m = new Mario(this);
+		map.addPlayer(m);
+	}
+
+	public synchronized void updateStage() {
+		map.act();
+		if (!gameOver && Coin.N_COINS == Coin.COINS_CATCHED) {
+			gameOver();
+			final Stage s = this;
+			new Thread(new Runnable() {
+				public void run() {
+					try {
+						Thread.sleep(2000);
+					} catch (Exception e) {
+					}
+					Coin.N_COINS = 0;
+					gameOver = false;
+					map.nextLevel();
+					map.addPlayer(new Mario(s));
+					Coin.COINS_CATCHED = 0;
+				}
+			}).start();
+		}
+	}
+
+	public synchronized void renderStage(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		map.paint(g);
+		// indicamos que se ha llegado al final
+		// Del juego si es necesario
+
+		if (gameOver) {
+			Graphics2D g2 = (Graphics2D) g;
+			g2.setColor(Color.WHITE);
+			g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_GASP);
+			g2.setFont(new Font(Font.MONOSPACED, Font.BOLD, 30));
+			g2.drawString("Fin del juego", WIDTH / 2 - 100, HEIGHT / 2 - 10);
+		}
+		// PAra simular ocuridad en el mapa
+		// BufferedImage b=
+		// new BufferedImage(WIDTH,HEIGHT,2);
+		// gg.setColor(Color.BLACK);
+		// gg.setPaint(new GradientPaint(WIDTH/2,0,color.BLACK,WIDTH/2,HEIGHT,
+		// Color.WHITE));
+		// gg.fillRect(0,0,WIDTH,HEIGHT);
+		// b=imgEffects.returnAlphaImg(b,0.6F);
+		// g.drawImage(b,0,0.null)
+	}
+
+	public synchronized void mouseMoved(MouseEvent e) {
+	}
+
+	public void keyPressed(KeyEvent e) {
+		for (int i = 0; i < map.players.size(); i++) {
+			map.players.get(i).keyPressed(e);
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+		for (int i = 0; i < map.players.size(); i++) {
+			map.players.get(i).keyReleased(e);
+		}
+	}
+
+	public void mousePressed(MouseEvent e) {
+	}
+
+	public float getGravity() {
+		return gravity;
+	}
+
+	public Map getCurrentMap() {
+		return map;
+	}
+
+	public static void main(String[] args) {
+		Main p = new Main();
+		p.getWindow().setVisible(true);
+		p.startGame();
+	}
+}
+//fin de la clase.
+	
