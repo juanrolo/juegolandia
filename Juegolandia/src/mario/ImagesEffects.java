44a171940d6c4bd4367a996df3d7e708c55d605a
Alberto Mena <albertomenadaw@gmail.com>
2018-03-28 12:15:06 +0200

    Clase imagesEffects acabada.

--- Juegolandia/src/mario/ImagesEffects.java
+++ Juegolandia/src/mario/ImagesEffects.java
@@ -70,4 +70,62 @@ public class ImagesEffects {
 
 	public BufferedImage returnRescaleImg(
 		BufferedImage img, int w, int h) {
-			
+		System. out . println ( img . getType()) ;
+		Bufferedlmage image = new Bufferedlmage (
+		w, h , img . getTypeQ ) ;
+		Graphics g = image. getGraphics () ;
+		g. drawlmage (img , 0, 0, w , h , null ) ;
+		return image ;
+		}
+	
+		public Bufferedlmage returnRotatedlmgC
+		Bufferedlmage img , int degrees) {
+		return returnRotatedImg (img , Math .toRadians(degrees)) ;
+		}
+		
+		public void paintShearlmg (Graphics g ,
+		String name , int x , int y , int w, int h ,
+		double shx , double shy) {
+		Graphics2D g2 = (Graphics2 D) g ;
+		g 2 . shear (shx , shy) ;
+		g2 . drawImage( imgLoader. getlmage(name) ,
+		x , y , w , h , null ) ;
+		g 2 . di sposeQ ;
+		}
+		
+		public void paintRotatedlmg(Graphics g ,
+		String name , int x , int y , int w,
+		int h , int degrees) {
+		Graphics2D g2 = (Graphics2 D) g ;
+		/*RenderingHints rh = new RenderingHints(
+		Rende ringHints. KEY_ANTIALIASING ,
+		RenderingHints. VAL U E_ANTIALIAS_ON) ;
+		rh . put ( RenderingHints. KEY_ RENDERING ,
+		RenderingHints. VALUE_RENDER_QUALITY) ;
+		g2 . setRende ringHints ( rh) ;*/
+		AffineTransform original = g2. getTransform () ;
+		AffineTransform rotate = new AffineTransform () ;
+		rotate. rotate (Math. toRadians(degrees) , x+w/2 , y+ h/2) ;
+		g2 . transform( rotate) ;
+		g2 . drawImage( imgLoader. getlmage(name) , x , y , w , h , null ) ;
+		g2 .transform(original ) ;
+		g2 . dispose () ;
+		}
+		
+		public Bufferedlmage returnRotatedImg (
+		Bufferedlmage img , double radians) {
+		Bufferedlmage image = new Bufferedlmage (
+		img . getwidth () , img. getHeight () ,
+		img . getColorModel () . getTransparencyO ) ;
+		Graphics2D g2 = (Graphics2 D) image. getGraphics() ;
+		AffineTransform rotation = new AffineTransform() ;
+		rotation. rotate( radians ,
+		img . getwidth0 / 2 , img . getHei ght0/2) ;
+		g2 .transform( rotation) ;
+		return image;
+		}
+		
+	}//fin de la clase ImagesEfects
+
+	//fin de ImagesEfects.java
+	
